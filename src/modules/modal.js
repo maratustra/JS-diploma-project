ab89388e84b89bd62b2ebd4ef8c03eabc2bee486
Alena Maramygina <maratustraa@gmail.com>
2021-11-17 19:20:16 +0500

    feat(modal.js, index.js): добавлена функция появления/скрытия модального окна

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,5 +1,22 @@
+/*jshint esversion: 6 */
+`use strict`;
+
 const modal = () => {
-  console.log('Hi there!');
+  const orderCallBtn = document.querySelector('.btn-block.fancyboxModal');
+  const modal = document.querySelector('.header-modal--opened');
+  const modalCloseBtn = document.querySelector('.header-modal__close');
+  const overlay = document.querySelector('.overlay');
+
+
+  orderCallBtn.addEventListener('click', (e) => {
+    overlay.style.display = 'block';
+    modal.style.display = 'block';
+  });
+
+  modalCloseBtn.addEventListener('click', () => {
+    overlay.style.display = 'none';
+    modal.style.display = 'none';
+  });
 };
 
 export default modal;
